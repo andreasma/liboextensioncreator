7e52309cdba55d36d37b15ebc5a9d909431ffd03
andreasma <maand@gmx.de>
2021-04-08 20:08:25 +0200

    add package dependencies to setup

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        'PyQt5',
+        'validators',
+        ],
     package_data={
         "": ["*.txt", "license_files/*.txt", "*.rst"],
         
