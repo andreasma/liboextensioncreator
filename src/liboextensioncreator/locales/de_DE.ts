974da818578defa2fa3bdd7b399b8019240df548
andreasma <maand@gmx.de>
2021-04-30 11:51:58 +0200

    files for German localization

---
+++ src/liboextensioncreator/locales/de_DE.ts
@@ -0,0 +1,223 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>CreatorApp</name>
+    <message>
+        <location filename="../liboextensioncreator.py" line="39"/>
+        <source>LibreOffice Extension Creator Dialog</source>
+        <translation>LibreOffice-Extension-Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>CreatorGroupWidget</name>
+    <message>
+        <location filename="../liboextensioncreator.py" line="62"/>
+        <source>LibreOffice Extension Creator</source>
+        <translation>LibreOffice-Extension-Ersteller</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="66"/>
+        <source>You can use this program to create a new non-code LibreOffice extension.</source>
+        <translation>Sie können dieses Programm zum Erstellen einer neuen Inhalte-Erweiterung für LibreOffice verwenden.</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="77"/>
+        <source>Once you are finished with the data fields, you could create the extension by clicking the OK button.</source>
+        <translation>Wenn Sie die Eingabefelder ausgefüllt haben, können Sie die Extension durch Klicken auf die OK-Schaltfläche erstellen.</translation>
+    </message>
+</context>
+<context>
+    <name>CreatorTabWidget</name>
+    <message>
+        <location filename="../liboextensioncreator.py" line="129"/>
+        <source>Name of the extension author / publisher</source>
+        <translation>Name des Extension-Autors / -Veröffentlichers</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="138"/>
+        <source>URL of the author&apos;s / publisher&apos;s  website or blog</source>
+        <translation>URL der Webseite oder des Blogs des Autors / Veröffentlichers</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="144"/>
+        <source>Version number of the extension (e.gl 0.1)</source>
+        <translation>Versionsnummer der Extension (z.B. 0.1)</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="152"/>
+        <source>Identifier</source>
+        <translation>Identifier</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="159"/>
+        <source>Displayed Name</source>
+        <translation>Angezeigter Name</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="164"/>
+        <source>Platform</source>
+        <translation>Plattform</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="191"/>
+        <source>Minimal LibreOffice version</source>
+        <translation>Minimale LibreOffice-Version</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="211"/>
+        <source>Description / Documentation of the Extension (*.txt) (English Language)</source>
+        <translation>Beschreibung / Dokumentation der Extension (*.txt) (Englische Sprache)</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="215"/>
+        <source>Choose File</source>
+        <translation>Datei auswählen</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="221"/>
+        <source>Choose an Icon for your extension, if you already created one.</source>
+        <translation>Ein Symbol für Ihre Extension auswählen, falls Sie bereits eines erstellt haben.</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="225"/>
+        <source>Choose an Icon</source>
+        <translation>Symbol auwählen</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="118"/>
+        <source>Name of your LibreOffice Extension, between 8 and 30 character</source>
+        <translation>Name Ihrer LibreOffice-Extension, 8 bis 30 Zeichen</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="239"/>
+        <source>Accepted by:</source>
+        <translation>Akzeptiert duch (Accepted by):</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="247"/>
+        <source>Choose a license for your extension:</source>
+        <translation>Wählen Sie eine Lizenz für Ihre Extension:</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="277"/>
+        <source>Which kind of content extension to build?</source>
+        <translation>Welche Art von Inhalte-Extension soll erstellt werden?</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="311"/>
+        <source>AutoCorrect Extension</source>
+        <translation>Autokorrektur-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="324"/>
+        <source>AutoText Extension</source>
+        <translation>Autotext-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="336"/>
+        <source>Gallery Extension</source>
+        <translation>Gallery-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="368"/>
+        <source>IconSet Extension</source>
+        <translation>Iconset-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="381"/>
+        <source>Palette Extension</source>
+        <translation>Farbpaletten-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="410"/>
+        <source>Template Extension</source>
+        <translation>Vorlagen-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="317"/>
+        <source>Choose the *.dat file for your AutoCorrect Extension</source>
+        <translation>Wählen Sie die *.dat Datei für Ihre Autokorrekt Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="319"/>
+        <source>Choose the *.dat file</source>
+        <translation>Wähle die *.dat Datei</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="329"/>
+        <source>Choose the *.bau file for your AutoText Extension</source>
+        <translation>Wählen Sie die *.bau Datei für Ihre Autotext-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="331"/>
+        <source>Choose the *.bau file</source>
+        <translation>Wähle die *.bau Datei</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="341"/>
+        <source>Choose the *.sdg file for your Gallery Extension</source>
+        <translation>Wählen Sie die *.sdg Datei für Ihre Gallery-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="344"/>
+        <source>Choose the *.sdg file</source>
+        <translation>Wähle die *.sdg Datei</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="348"/>
+        <source>Choose the *.sdv file for your Gallery Extension</source>
+        <translation>Wählen Sie die *.sdv Datei für Ihre Gallery Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="351"/>
+        <source>Choose the *.sdv file</source>
+        <translation>Wähle die *.sdv Datei</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="355"/>
+        <source>Choose the *.thm file for your Gallery Extension</source>
+        <translation>Wählen Sie die *.thm Datei für Ihre Gallery-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="358"/>
+        <source>Choose the *.thm file</source>
+        <translation>Wähle die *.thm Datei</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="373"/>
+        <source>Choose the *.zip archive for your IconSet Extension</source>
+        <translation>Wählen Sie das *.zip Archiv für Ihre Iconset-Extension</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="417"/>
+        <source>Choose the *.zip archive</source>
+        <translation>Wähle das *.zip Archiv</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="386"/>
+        <source>Name of the Palette</source>
+        <translation>Name der Farbpalette</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="388"/>
+        <source>Palette Name</source>
+        <translation>Farbpalettenname</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="392"/>
+        <source>Number of Colors for the Palette, up to 15.</source>
+        <translation>Anzahl der Farben in der Farbpalette, bis zu 15.</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="400"/>
+        <source>Insert per box/line below &apos;color name, hexadecimal number&apos; without the hash &apos;#&apos;</source>
+        <translation>Fügen Sie pro Box / Zeile unten ein: &quot;Farbname, Hexadecimal-Nummer&apos; ohne den Hash &quot;#&quot;</translation>
+    </message>
+    <message>
+        <location filename="../liboextensioncreator.py" line="415"/>
+        <source>Choose the *.zip archive for your Template Extension</source>
+        <translation>Wählen Sie das *zip Archiv für Ihre Vorlagen-Extension</translation>
+    </message>
+</context>
+</TS>
